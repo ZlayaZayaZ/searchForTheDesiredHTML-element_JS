5bf08ff87ac94b37fdd029cbe71be93513cafb20
Vasilkova Lyubov <damirana_@mail.ru>
2022-12-11 00:09:46 +0700

    Введение отдельной функции для активного элемента

--- Слайдер изображений/task.js
+++ Слайдер изображений/task.js
@@ -3,17 +3,22 @@ dots = document.querySelectorAll('.slider__dot')
 
 let listSliders = Array.from(sliders),
 len = listSliders.length,
-index = listSliders.indexOf(document.querySelector('.slider__item_active'))
 
+index = listSliders.indexOf(document.querySelector('.slider__item_active'))
 dots[index].className = 'slider__dot slider__dot_active'
+
+function activeSlide (indexNew) {
+    let indexPastActive = listSliders.indexOf(document.querySelector('.slider__item_active'))
+    sliders[indexPastActive].className = 'slider__item'
+    dots[indexPastActive].className = 'slider__dot'
+    sliders[indexNew].className = 'slider__item slider__item_active'
+    dots[indexNew].className = 'slider__dot slider__dot_active'
+    index = indexNew
+}
 
 for (let i = 0; i < len; i++) {
     dots[i].onclick = () => {
-        sliders[index].className = 'slider__item'
-        dots[index].className = 'slider__dot'
-        index = i
-        sliders[index].className = 'slider__item slider__item_active'
-        dots[index].className = 'slider__dot slider__dot_active'
+        activeSlide (i)
     }
 }
 
@@ -21,23 +26,17 @@ const prev = document.querySelector('.slider__arrow_prev'),
 next = document.querySelector('.slider__arrow_next')
 
 prev.onclick = () => {
-    sliders[index].className = 'slider__item'
-    dots[index].className = 'slider__dot'
     index = index - 1
     if (index < 0) {
         index = index + len
     }
-    sliders[index].className = 'slider__item slider__item_active'
-    dots[index].className = 'slider__dot slider__dot_active'
+    activeSlide (index)
 }
 
 next.onclick = () => {
-    sliders[index].className = 'slider__item'
-    dots[index].className = 'slider__dot'
     index = index + 1
     if (index > len - 1) {
         index = index - len
     }
-    sliders[index].className = 'slider__item slider__item_active'
-    dots[index].className = 'slider__dot slider__dot_active'
+    activeSlide (index)
 }
